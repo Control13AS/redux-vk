691ab71eb8967b3baea7f377a72a12fa0f117e19
Control13AS <65903088+Control13AS@users.noreply.github.com>
2020-10-05 15:34:02 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/configureStore'
+import App from './containers/App'
+
+import * as serviceWorker from './serviceWorker';
+//import registerServiceWorker from './registerServiceWorker'
+
+import './index.css'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+serviceWorker.unregister();
+//registerServiceWorker()
+
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,4 +34,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
